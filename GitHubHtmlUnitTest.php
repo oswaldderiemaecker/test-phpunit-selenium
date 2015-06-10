47be47c0ee9d4764767a7bb5ef9d9a234ebce6bf
Oswald De Riemaecker <oswald@continuousphp.com>
2015-06-10 17:40:23 +0200

    Initial commit

---
+++ GitHubHtmlUnitTest.php
@@ -0,0 +1,26 @@
+<?php
+class GitHubHtmlUnitTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * @var \RemoteWebDriver
+     */
+    protected $webDriver;
+
+    public function setUp()
+    {
+        $capabilities = array(\WebDriverCapabilityType::BROWSER_NAME => 'firefox');
+        $capabilities = array(\WebDriverCapabilityType::BROWSER_NAME => 'htmlunit');
+        $this->webDriver = RemoteWebDriver::create('http://localhost:4444/wd/hub', $capabilities);
+    }
+
+    protected $url = 'https://github.com';
+
+    public function testGitHubHomeHtmlUnit()
+    {
+        $this->webDriver->get($this->url);
+        // checking that page title contains word 'GitHub'
+        $this->assertContains('GitHub', $this->webDriver->getTitle());
+    }
+
+}
+?>
